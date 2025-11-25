1d5315d7546ecad8bb5e6900be556941cce56389
strhodler <strhodler@proton.me>
2025-11-25 19:48:09 +0100

    fix(wallets): prevent refreshing all wallets when adding a new one.

--- app/src/main/java/com/strhodler/utxopocket/presentation/wallets/add/AddWalletViewModel.kt
+++ app/src/main/java/com/strhodler/utxopocket/presentation/wallets/add/AddWalletViewModel.kt
@@ -292,7 +292,7 @@ class AddWalletViewModel @Inject constructor(
                                 nodeSnapshot.status is NodeStatus.Synced &&
                                 nodeSnapshot.network == result.wallet.network
                             ) {
-                                runCatching { walletRepository.refresh(result.wallet.network) }
+                                runCatching { walletRepository.refreshWallet(result.wallet.id) }
                             }
                         }
                         _events.emit(AddWalletEvent.WalletCreated(result.wallet))
