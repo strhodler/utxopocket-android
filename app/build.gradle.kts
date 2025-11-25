8e0f9a02d8efa8ad7b835785b28087805d8049bd
strhodler <strhodler@proton.me>
2025-11-26 00:21:30 +0100

    chore(release): bump app version to v0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.strhodler.utxopocket"
         minSdk = 28
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.7.0"
+        versionCode = 9
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
